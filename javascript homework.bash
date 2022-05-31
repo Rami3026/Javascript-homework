0d5818bf03f7bcdfba1ef586d4997091b6307ae1
Rami3026 <91858420+Rami3026@users.noreply.github.com>
2022-05-31 16:50:35 +0100

    Add files via upload

---
+++ javascript homework.bash
@@ -0,0 +1,26 @@
+$ cd my_clone
+bash: cd: my_clone: No such file or directory
+$ cd maryrose-hw
+$ git branch mich
+$ git checkout mich
+Switched to branch 'mich'
+$ git log
+commit f6f5b1a0298d9ac0c257ebea665e56a7effc1557
+Author: danasselin <johndoe@example.com>
+Date:   Thu Nov 19 16:30:26 2015 -0500
+
+    Finish my homework
+
+commit 04107e57402323d48bbd2e1692a3ba04940dd89e
+Author: danasselin <johndoe@example.com>
+Date:   Thu Nov 19 16:23:15 2015 -0500
+
+    Write if/else statement
+
+commit d8570f2181b57e94cacc8021dbc0a917897f2efe
+Author: danasselin <johndoe@example.com>
+Date:   Thu Nov 19 16:18:08 2015 -0500
+
+    add homework.js
+$ cd ../maryrose-hw
+$ 
